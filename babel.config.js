efda8e2e05d53f18a4c50042075c0459f1f3cc81
Fukuyoshi, Sakura <sakura.fukuyoshi@accenture.com>
2023-01-19 11:19:56 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,7 @@ module.exports = {
       'module-resolver',
       {
         alias: {
-          'react-native-month-year-picker': '../src',
+          'react-native-month-year-picker-gregorian': '../src',
         },
       },
     ],
